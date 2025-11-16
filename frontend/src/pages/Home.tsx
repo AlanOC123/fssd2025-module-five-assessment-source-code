aa0497a72e696de729f7ccc09b1c578e7feb36a0
Alan O'Connor <aocthatsme246@gmail.com>
2025-11-16 20:10:33 +0000

    Scaffolded basic routing and set up core directory structure

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export function Home() {
+    return (
+        <div>
+            <h1>Hello from the Home Page!</h1>
+            <p>Router is working...</p>
+        </div>
+    )
+}

0a00ea4f5705dd3b7e565e4b2cb9e164dcd89aad
Alan O'Connor <aocthatsme246@gmail.com>
2026-02-05 20:32:32 +0000

    Updated settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -250,17 +250,20 @@ SECURE_HSTS_PRELOAD = True
 # --- 8. Email Configuration ---
 # Uses Gmail SMTP for sending password reset emails.
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND = config("EMAIL_BACKEND", default="django.core.mail.backends.smtp.EmailBackend")
 EMAIL_HOST = config("EMAIL_HOST", default="smtp.gmail.com")
 EMAIL_PORT = config("EMAIL_PORT", default=587, cast=int)
 EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=True, cast=bool)
 EMAIL_USE_SSL = config("EMAIL_USE_SSL", default=False, cast=bool)
 
 if IS_PROD:
-    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
     EMAIL_HOST_USER = config("EMAIL_HOST_USER")
     EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
     DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
+
+    EMAIL_PORT = config("EMAIL_PORT", cast=int)
+    EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool)
+    EMAIL_USE_SSL = config("EMAIL_USE_SSL", cast=bool)
 else:
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
     EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
